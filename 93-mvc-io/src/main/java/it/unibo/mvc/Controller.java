2de02fb4795b8fd2e613647711d822698a722f91
nicholas benedetti <nicholas.benedetti@studio.unibo.it>
2024-11-19 16:07:42 +0100

    ex-93: created Controller interface and implemented it

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,37 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * simple controller responsible of I/O access.
+ * It considers only the standard output, and it is able to print on it.
  */
 public interface Controller {
 
+    /**
+     * Sets the next string to print.
+     *
+     * @param stringToSet the string to set.
+     *
+     * @throws NullPointerException if the string passed as input in null.
+     */
+    public void setString(final String stringToSet);
+
+    /**
+     * @return The next string to print
+     */
+    public String getSettedString();
+
+    /**
+     *
+     * @return A List<String> with all the past strings that were printed.
+     */
+    public List<String> getPrintHistory();
+
+    /**
+     * Prints the current setted string in the STDOUT.
+     *
+     * @throws IllegalStateException if the string is not set.
+     */
+    public void printSettedString();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,44 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
- * 
+ *
  *
  */
 public final class SimpleController implements Controller {
+    private String currSettedString = null;
+    private List<String> printHistory = new ArrayList<>();
 
+    @Override
+    public void setString(String stringToSet) {
+        try {
+            this.currSettedString = Objects.requireNonNull(stringToSet);
+        } catch (NullPointerException e) {
+            throw new NullPointerException();
+        }
+    }
+
+    @Override
+    public String getSettedString() {
+        return currSettedString;
+    }
+
+    @Override
+    public List<String> getPrintHistory() {
+        return printHistory;
+    }
+
+    @Override
+    public void printSettedString() {
+        try {
+            System.out.println(Objects.requireNonNull(currSettedString));
+        } catch (NullPointerException e) {
+            throw new IllegalStateException("String has not been setted");
+        }
+        this.printHistory.add(this.currSettedString);
+        this.currSettedString = null;
+    }
 }
